6c5148da9ab5b3b8ec0023af35afd567398b2cb9
Joel-Groves <jgroves@outlook.com.au>
2023-10-31 01:44:51 +1100

    Created working networking with link to an API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,11 +32,65 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  List _resources = [];
+
+  void _fetchResources() async {
+    final response = await http.get(Uri.parse('https://api.sampleapis.com/codingresources/codingResources'));
+    if (response.statusCode == 200)
+    {
+      final data = json.decode(response.body) as List;
+      setState(() {
+        _resources = data;
+      });
+    }
+    else 
+    {
+      throw Exception('Failed to load resoureces');
+    }
+  }
+
+  _launchURL(String url) async 
+  {
+    if (await canLaunch(url)) 
+    {
+      await launch(url);
+    }
+    else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  void initState()
+  {
+    super.initState();
+    _fetchResources();
+  }
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-    );
-  }
-}
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: _resources.isEmpty
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: _resources.length,
+                itemBuilder: (context, index) {
+                  final resource = _resources[index];
+                  return InkWell(
+                      onTap: () => {_launchURL(resource['url'])},
+                      child: Card(
+                          child: ListTile(
+                        title: Text(resource['description']),
+                        subtitle: Text(resource['url']),
+                        leading: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
+                        trailing: Text(resource['types'].join(', ')),
+                      )));
+                }));
+  }}
